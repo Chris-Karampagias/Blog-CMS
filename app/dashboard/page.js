0ae55d3871f3ab51edafdd0b791294f0d983ff9f
Chris <karampagiaschris@gmail.com>
2023-10-14 18:29:22 +0300

    Add icons to options

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,7 +3,8 @@ import { useContext, useEffect } from "react";
 import { AuthContext } from "@/utils/AuthContextProvider";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import toast, { Toaster } from "react-hot-toast";
+import Icon from "@mdi/react";
+import { mdiWrenchCog, mdiCardPlus } from "@mdi/js";
 
 export default function Page() {
   const context = useContext(AuthContext);
@@ -16,17 +17,10 @@ export default function Page() {
   return (
     <>
       {context.user && (
-        <div className="flex flex-col items-center">
-          <div className="chat mt-10 space-y-3 absolute bottom-2 chat-start self-start text-3xl">
-            <div className="chat-bubble bg-gray-400 text-slate-100">
-              Nice having you back admin!
-            </div>
-            <div className="chat-bubble bg-gray-400 text-slate-100">
-              Welcome to the Dashboard!
-            </div>
-          </div>
+        <div className="flex flex-col relative scroll-dis items-center">
           <div className="flex flex-col min-h-screen items-center gap-10 mt-10">
             <div className="card w-full bg-base-100 shadow-2xl">
+              <Icon path={mdiWrenchCog} size={4} className="self-center" />
               <div className="card-body space-y-2 p-10">
                 <h2 className="card-title text-2xl">Posts</h2>
                 <p className="text-xl">View, edit & delete your posts</p>
@@ -38,6 +32,7 @@ export default function Page() {
               </div>
             </div>
             <div className="card w-full bg-base-100 shadow-2xl">
+              <Icon path={mdiCardPlus} size={4} className="self-center" />
               <div className="card-body space-y-2 p-10">
                 <h2 className="card-title text-2xl">New Post</h2>
                 <p className="text-xl">Create a new post</p>
@@ -47,6 +42,12 @@ export default function Page() {
                   </Link>
                 </div>
               </div>
+            </div>
+          </div>
+          <div className="chat space-y-3  hidden absolute xl:block top-10 left-3 chat-start self-start text-3xl">
+            <div className="chat-bubble bg-gray-400 text-slate-100">
+              Nice having you back admin! <br />
+              Welcome to the Dashboard!
             </div>
           </div>
         </div>
