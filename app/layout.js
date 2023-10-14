3aee1480a6f9ac00a8540fda1a1cc33501b47b18
Chris <karampagiaschris@gmail.com>
2023-10-14 13:28:26 +0300

    Create context for authentication status

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,14 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-export default function RootLayout({ children }) {
+import { AuthContextProvider } from "@/utils/AuthContextProvider";
+export default function Layout({ children }) {
   return (
     <html data-theme="winter" lang="en">
       <body className="min-h-screen">
-        <Navbar loggedIn={false} />
-        {children}
+        <AuthContextProvider>
+          <Navbar />
+          {children}
+        </AuthContextProvider>
       </body>
     </html>
   );
