a9b6c6e0b9b4d796c1ae053d3a1f1bf8b064760a
Chris <karampagiaschris@gmail.com>
2023-10-15 20:32:56 +0300

    Add Head to layout

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,15 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 import { AuthContextProvider } from "@/utils/AuthContextProvider";
 export default function Layout({ children }) {
   return (
     <html data-theme="winter" lang="en">
+      <Head>
+        <title>Blog CMS</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body className="min-h-screen">
         <AuthContextProvider>
           <Navbar />
