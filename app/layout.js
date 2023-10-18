f9c905a70070d999e70db226bc40bda990592020
Chris <karampagiaschris@gmail.com>
2023-10-18 11:57:05 +0300

    Migrate from Head to metadata

--- app/layout.js
+++ app/layout.js
@@ -3,14 +3,22 @@ import Navbar from "@/components/Navbar";
 import Head from "next/head";
 import { AuthContextProvider } from "@/utils/AuthContextProvider";
 import { Toaster } from "react-hot-toast";
+
+export const metadata = {
+  title: "Blog CMS",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  other: {
+    charset: "UTF-8",
+  },
+};
+
 export default function Layout({ children }) {
   return (
     <html data-theme="winter" lang="en">
-      <Head>
-        <title>Blog CMS</title>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <body className="min-h-screen">
         <AuthContextProvider>
           <Navbar />
