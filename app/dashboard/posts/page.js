5ef4b6898e0840b177d3e4ef94037b10c063ae78
Chris <karampagiaschris@gmail.com>
2023-10-20 19:10:38 +0300

    Add notification when there are no posts

--- app/dashboard/posts/page.js
+++ app/dashboard/posts/page.js
@@ -69,54 +69,76 @@ export default function Posts() {
         <span className="loading loading-spinner absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] loading-lg "></span>
       )}
       {context.user && !loading && (
-        <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-2 min-[2500px]:grid-cols-3 justify-items-center gap-10 gap-y-28  mt-10 p-10 min-h-screen">
-          {posts.map((post) => {
-            return (
-              <div
-                key={post._id}
-                className="card max-w-[600px] lg:aspect-square bg-base-100 shadow-2xl"
+        <>
+          {posts.length > 0 && (
+            <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-2 min-[2500px]:grid-cols-3 justify-items-center gap-10 gap-y-28  mt-10 p-10 min-h-screen">
+              {posts.map((post) => {
+                return (
+                  <div
+                    key={post._id}
+                    className="card max-w-[600px] lg:aspect-square bg-base-100 shadow-2xl"
+                  >
+                    <figure>
+                      <Image
+                        src={`https://blog-api-production-a764.up.railway.app/${post.image}`}
+                        width={600}
+                        height={600}
+                        alt="Post image"
+                      />
+                    </figure>
+                    <div className="card-body justify-between  p-5 md:p-10 space-y-5">
+                      <div>
+                        <h2 className="card-title self-center md:self-start text-2xl md:text-3xl">
+                          {post.title}
+                        </h2>
+                        <p className="text-slate-500 mt-3 lg:text-xl">
+                          <span className="font-bold">Posted: </span>
+                          {formatDate(post.postedAt)}
+                        </p>
+                        <p className="text-slate-500 mt-3 lg:text-xl">
+                          <span className="font-bold">Last edit: </span>
+                          {formatDate(post.updatedAt)}
+                        </p>
+                      </div>
+                      <div className="card-actions justify-center gap-5 md:justify-end">
+                        <Link
+                          href={"/dashboard/posts/update/" + post._id}
+                          className="btn btn-secondary btn-md xl:btn-lg"
+                        >
+                          Edit
+                        </Link>
+                        <Link
+                          href={"/dashboard/posts/delete/" + post._id}
+                          className="btn btn-error bg-red-500 btn-md xl:btn-lg"
+                        >
+                          Delete
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+          {posts.length === 0 && (
+            <div className="alert">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="stroke-info shrink-0 w-6 h-6"
               >
-                <figure>
-                  <Image
-                    src={`https://blog-api-production-a764.up.railway.app/${post.image}`}
-                    width={600}
-                    height={600}
-                    alt="Post image"
-                  />
-                </figure>
-                <div className="card-body justify-between  p-5 md:p-10 space-y-5">
-                  <div>
-                    <h2 className="card-title self-center md:self-start text-2xl md:text-3xl">
-                      {post.title}
-                    </h2>
-                    <p className="text-slate-500 mt-3 lg:text-xl">
-                      <span className="font-bold">Posted: </span>
-                      {formatDate(post.postedAt)}
-                    </p>
-                    <p className="text-slate-500 mt-3 lg:text-xl">
-                      <span className="font-bold">Last edit: </span>
-                      {formatDate(post.updatedAt)}
-                    </p>
-                  </div>
-                  <div className="card-actions justify-center gap-5 md:justify-end">
-                    <Link
-                      href={"/dashboard/posts/update/" + post._id}
-                      className="btn btn-secondary btn-md xl:btn-lg"
-                    >
-                      Edit
-                    </Link>
-                    <Link
-                      href={"/dashboard/posts/delete/" + post._id}
-                      className="btn btn-error bg-red-500 btn-md xl:btn-lg"
-                    >
-                      Delete
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+              <span>No posts yet!</span>
+            </div>
+          )}
+        </>
       )}
     </>
   );
