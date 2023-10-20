0d89b38f0add6c1382afa7357a02bf4f9f3899d4
Chris <karampagiaschris@gmail.com>
2023-10-20 19:12:36 +0300

    edit

--- app/dashboard/posts/page.js
+++ app/dashboard/posts/page.js
@@ -135,7 +135,7 @@ export default function Posts() {
                   d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 ></path>
               </svg>
-              <span>No posts yet!</span>
+              <span className="md:text-2xl">No posts yet!</span>
             </div>
           )}
         </>
